b2fb44a42a4ec14400b45e6f20dd10eb2dfd12a4
chenduowen233 <906432161@qq.com>
2021-08-23 17:13:46 +0800

    Site updated: 2021-08-23 17:13:46

--- js/Beautify.js
+++ js/Beautify.js
@@ -23,6 +23,6 @@ document.addEventListener('visibilitychange', function() {
 
 // 添加八毛卡通人物
 /*右下角添加卡通人物*/
-var bamao = '<i class="fas fa-arrow-up" style ="padding:8px" ></i><img style="max-width: 234%;transform: translate(-65px,-65px);" src="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js" title="回到顶部" data-ll-status="loaded" class="loaded">';
+var bamao = '<i class="fas fa-arrow-up" style ="padding:8px" ></i><img style="max-width: 234%;transform: translate(-65px,-65px);" src=https://cdn.jsdelivr.net/gh/fudalijunyi/picture-bed/img/20200629182853.gif"" title="回到顶部" data-ll-status="loaded" class="loaded">';
 /*添加到返回顶部按钮下*/
 document.getElementById("go-up").innerHTML = bamao
